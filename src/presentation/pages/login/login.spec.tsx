c7b801314a478b7ce52b6fd7dcd97e586b291712
estevam.rodrigues <estevam.rodrigues@itbam.org.br>
2021-01-12 14:55:58 -0400

    test: ensure prevent Login to call submit more than once

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -112,4 +112,11 @@ describe('Login Component', () => {
       password
     })
   })
+
+  test('Should call Authentication only one', () => {
+    const { sut, authenticationSpy } = makeSut()
+    simulateValidSubmit(sut)
+    simulateValidSubmit(sut)
+    expect(authenticationSpy.callsCount).toBe(1)
+  })
 })

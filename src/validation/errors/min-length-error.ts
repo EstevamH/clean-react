227d2b7ca850c93d9b82c95041c8bb078cec8749
estevam.rodrigues <estevam.rodrigues@itbam.org.br>
2021-01-18 19:02:13 -0400

    feat: ensure MinLengthValidation returns falsy if value is valid

--- src/validation/errors/min-length-error.ts
+++ src/validation/errors/min-length-error.ts
@@ -1,5 +1,5 @@
 export class MinLengthError extends Error {
-  constructor (private readonly minLength: number) {
+  constructor (readonly minLength: number) {
     super(`O campo deve possuir no mínimo ${minLength} caracteres`)
     this.name = 'MinLengthError'
   }

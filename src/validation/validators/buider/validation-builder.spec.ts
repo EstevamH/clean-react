744c19274beba3e11cb6a156b158be96e5c4726a
estevam.rodrigues <estevam.rodrigues@itbam.org.br>
2021-01-26 07:58:04 -0400

    refactor: change comparison method in ValidationBuilder tests

--- src/validation/validators/buider/validation-builder.spec.ts
+++ src/validation/validators/buider/validation-builder.spec.ts
@@ -6,27 +6,27 @@ describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
     const field = faker.database.column()
     const validations = sut.field(field).required().build()
-    expect(validations).toEqual([new RequiredFieldValidation(field)])
+    expect(validations).toStrictEqual([new RequiredFieldValidation(field)])
   })
 
   test('Should return EmailValidation', () => {
     const field = faker.database.column()
     const validations = sut.field(field).email().build()
-    expect(validations).toEqual([new EmailValidation(field)])
+    expect(validations).toStrictEqual([new EmailValidation(field)])
   })
 
   test('Should return MinLengthValidation', () => {
     const field = faker.database.column()
     const length = faker.random.number()
     const validations = sut.field(field).min(length).build()
-    expect(validations).toEqual([new MinLengthValidation(field, length)])
+    expect(validations).toStrictEqual([new MinLengthValidation(field, length)])
   })
 
   test('Should return a list of validations', () => {
     const field = faker.database.column()
     const length = faker.random.number()
     const validations = sut.field(field).required().min(length).email().build()
-    expect(validations).toEqual([
+    expect(validations).toStrictEqual([
       new RequiredFieldValidation(field),
       new MinLengthValidation(field, length),
       new EmailValidation(field)
